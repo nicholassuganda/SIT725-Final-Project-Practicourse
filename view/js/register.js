a6f8da1b6453413ecd53b453b3983c948996736b
Nicholas Suganda <sugandanicholas@gmail.com>
2024-09-27 21:54:21 +1000

    add aboutUs page, remove contactUs nav button, move register.html style to styles.css, add register.js

---
+++ view/js/register.js
@@ -0,0 +1,6 @@
+const registerForm = document.getElementById('registerForm');
+
+registerForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Registered successfully!');
+});
